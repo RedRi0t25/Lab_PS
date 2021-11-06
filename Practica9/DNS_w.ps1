ecc07c011d370b1b77ffc1b687e3c318bd41fe59
RedRi0t25 <89947663+RedRi0t25@users.noreply.github.com>
2021-11-06 14:18:11 -0600

    Update DNS_w.ps1

--- Practica9/DNS_w.ps1
+++ Practica9/DNS_w.ps1
@@ -1,4 +1,6 @@
-﻿$dns =  ipconfig /displaydns 
+#Practica9
+#DNS
+$dns =  ipconfig /displaydns 
 $Path = "C:\Users\vgrob\Downloads\laboratorio xd\Practica010\RDNS.txt"
 $codificado = [Convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes($dns))
 Set-Content -Value "$codificado" -Path $Path
