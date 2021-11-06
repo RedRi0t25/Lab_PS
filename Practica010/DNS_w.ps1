76cf8b03de54805cc53cab4c60c89174d974d063
RedRi0t25 <89947663+RedRi0t25@users.noreply.github.com>
2021-11-06 14:16:15 -0600

    Delete Practica010 directory

--- Practica010/DNS_w.ps1
+++
@@ -1,4 +0,0 @@
-﻿$dns =  ipconfig /displaydns 
-$Path = "C:\Users\vgrob\Downloads\laboratorio xd\Practica010\RDNS.txt"
-$codificado = [Convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes($dns))
-Set-Content -Value "$codificado" -Path $Path
