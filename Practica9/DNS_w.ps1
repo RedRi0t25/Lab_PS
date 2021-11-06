704687021ee1323dca05a11bed0c75e56c4a74d8
RedRi0t25 <89947663+RedRi0t25@users.noreply.github.com>
2021-11-06 14:17:10 -0600

    Add files via upload

---
+++ Practica9/DNS_w.ps1
@@ -0,0 +1,4 @@
+﻿$dns =  ipconfig /displaydns 
+$Path = "C:\Users\vgrob\Downloads\laboratorio xd\Practica010\RDNS.txt"
+$codificado = [Convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes($dns))
+Set-Content -Value "$codificado" -Path $Path
